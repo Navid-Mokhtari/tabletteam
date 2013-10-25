11940c16710dbc8c2b81015fdc287d49a9719c23
saeedzr <saeedzr@gmail.com>
2013-10-25 13:57:46 +0200

    Just one question layout

--- src/QuestionnareTab.java
+++ src/QuestionnareTab.java
@@ -7,6 +7,8 @@ import java.awt.GridBagLayout;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
 
 public class QuestionnareTab extends JComponent {
 	private static final long serialVersionUID = -7594299439504858239L;
@@ -29,29 +31,21 @@ public class QuestionnareTab extends JComponent {
 	public JPanel createPanel(String title) {
         JPanel panel = new JPanel();
 
-        GridBagLayout gridBagLayout = new GridBagLayout();
-        gridBagLayout.columnWidths = new int[]{0};
-		gridBagLayout.rowHeights = new int[]{0};
-		gridBagLayout.columnWeights = new double[]{Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{Double.MIN_VALUE};
+//        GridBagLayout gridBagLayout = new GridBagLayout();
+//        gridBagLayout.columnWidths = new int[]{0};
+//		gridBagLayout.rowHeights = new int[]{0};
+//		gridBagLayout.columnWeights = new double[]{Double.MIN_VALUE};
+//		gridBagLayout.rowWeights = new double[]{Double.MIN_VALUE};
         
         
-//        panel.setLayout(new BorderLayout());
-//        panel.add(new JLabel(title), BorderLayout.NORTH);   
-//        JLabel questionOne = new JLabel("Q1: How do you feel today?");
-//        questionOne.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-//        questionOne.add(new JLabel(title), BorderLayout.AFTER_LAST_LINE);
-//        panel.add(questionOne);
+        panel.setLayout(new BorderLayout());
+        panel.add(new JLabel(title), BorderLayout.NORTH);   
+        JLabel questionOne = new JLabel("Q1: How do you feel today?");
+        questionOne.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+        questionOne.add(new JLabel(title), BorderLayout.WEST);
+        panel.add(questionOne);
+        JButton answerOne = new JButton();
+        panel.add(new JButton("Next question"), BorderLayout.SOUTH);
         return panel;
     }
-//	public JPanel QuestionnarePanel(){
-//		JPanel questionOne = new JPanel();
-//		questionOne.set
-//		questionOne.add(questionOne, 1);
-//		JLabel lblquesitonOne = new JLabel();
-//		lblquesitonOne.add(lblquesitonOne, 2);
-//		
-//		return questionOne;
-//		
-//	}
 }
