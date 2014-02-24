d4cd36df2d3322796fcf8162c71de218acad88b6
Saeed Zanderahimi <saeedzr@gmail.com>
2014-02-24 12:58:00 +0100

    Added cancel button to questionnaires Changed the color of review page of daily questionnaire

--- src/main/java/app/CatQuestionDial.java
+++ src/main/java/app/CatQuestionDial.java
@@ -1,13 +1,20 @@
 package app;
 import java.awt.BorderLayout;
-
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 
 public class CatQuestionDial extends JDialog {
+	
+	private final JPanel contentPanel = new JPanel();
 
 	/**
 	 * 
@@ -33,7 +40,27 @@ public class CatQuestionDial extends JDialog {
 	public CatQuestionDial() {
 		setBounds(0, 0, 1366, 768);
 		
-		getContentPane().setLayout(new BorderLayout());
+		getContentPane().setLayout(new BorderLayout());		
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		getContentPane().add(contentPanel, BorderLayout.SOUTH);
+		
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			
+			JButton cancelButton = new JButton("Cancel");
+			cancelButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+			buttonPane.add(cancelButton);
+			cancelButton.addActionListener(new ActionListener() {
+					
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					dispose();
+					}
+			});
 			
 		JPanel panel12 = new JPanel();
 		getContentPane().add(panel12, BorderLayout.CENTER);
--- src/main/java/app/QuestionDial.java
+++ src/main/java/app/QuestionDial.java
@@ -1,15 +1,23 @@
 package app;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import app.QuestionnareTab;
 
 import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 
 public class QuestionDial extends JDialog {
+	
+	private final JPanel contentPanel = new JPanel();
 
 	/**
 	 * 
@@ -36,6 +44,27 @@ public class QuestionDial extends JDialog {
 		setBounds(0, 0, 1366, 768);
 		
 		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		getContentPane().add(contentPanel, BorderLayout.SOUTH);
+		
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			
+			JButton cancelButton = new JButton("Cancel");
+			cancelButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+			buttonPane.add(cancelButton);
+			cancelButton.addActionListener(new ActionListener() {
+					
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					dispose();
+					}
+			});
+		
 			
 			
 		JPanel panel12 = new JPanel();
@@ -48,6 +77,8 @@ public class QuestionDial extends JDialog {
 				e.printStackTrace();
 			}
 		panel12.setVisible(true);
+		
+		
 		getContentPane().add(panel12);
 					
 	}
