cc258d56c32898330ed648facb1245a268784787
Vladimir <vladimir.shulgin@gmail.com>
2014-03-03 19:50:48 +0100

    Closes #13, now config is outside the jar

--- src/main/java/app/HealthProperties.java
+++ src/main/java/app/HealthProperties.java
@@ -1,18 +1,29 @@
 package app;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class HealthProperties {
-	private static Properties properties=new Properties();
+	private static Properties properties = new Properties();
 
 	public void loadProperties() {
 		try {
-			properties.load(getClass().getClassLoader().getResourceAsStream(
-					"config.properties"));
-//			System.out.println("Loaded successfully");
+			FileInputStream file;
+			String path = "./config.properties";
+			file = new FileInputStream(path);
+			properties.load(file);
+			file.close();
 		} catch (IOException e) {
-			System.out.println("File with properties doesn't exist or was not loaded correctly");
+			System.out.println("File was not found");
+			try {
+				properties.load(getClass().getClassLoader()
+						.getResourceAsStream("config.properties"));
+			} catch (IOException e1) {
+				System.out
+						.println("File with properties doesn't exist or was not loaded correctly"
+								+ e1.toString());
+			}
 		}
 	}
 
